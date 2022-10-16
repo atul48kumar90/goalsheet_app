9d12a3b2f5fbe170e1a084addc72c02dd92f546d
Atul Kr Karn <atul48kumar90@gmail.com>
2022-10-16 22:42:56 +0530

    generate jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,7 +40,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToekn(user._id)
         })
     } else{
         res.status(400)
@@ -50,12 +51,34 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 const loginUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'login users'})
+    const { email, password } = req.body;
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password)))
+    {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToekn(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+    //res.status(200).json({message: 'login users'})
 })
 
 const getMe = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'user data'})
 })
+
+//generate JWT
+const generateToekn = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
 
 module.exports = {
     registerUser, loginUser, getMe
